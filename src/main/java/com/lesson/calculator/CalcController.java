cabea9bef00ce8bdac79a33a43da69161a351fb5
Professional <new99kpicem@gmail.com>
2023-05-03 23:45:25 +0300

    initial

--- src/main/java/com/lesson/calculator/CalcController.java
+++ src/main/java/com/lesson/calculator/CalcController.java
@@ -1,33 +1,37 @@
 package com.lesson.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalcController {
-    @GetMapping(path = "/calculator")
+    @GetMapping("/")
     public String hello() {
         return "Добро пожаловать в калькулятор";
     }
-    @GetMapping(path = "/calculator/plus")
+    @GetMapping(path = "/plus")
     public String plus(@RequestParam int num1, int num2) {
         int num3=num1+num2;
         return num1 + "+" + num2 + "=" + num3;
     }
-    @GetMapping(path = "/calculator/minus")
+    @GetMapping(path = "/minus")
     public String minus(@RequestParam int num1, int num2) {
         int num3=num1-num2;
         return num1 + "-" + num2 + "=" + num3;
     }
-    @GetMapping(path = "/calculator/multiply")
+    @GetMapping(path = "/multiply")
     public String multiply(@RequestParam int num1, int num2) {
         int num3=num1*num2;
         return num1 + "*" + num2 + "=" + num3;
     }
-    @GetMapping(path = "/calculator/divide")
+    @GetMapping(path = "/divide")
     public String divide(@RequestParam double num1, double num2) {
         double num3=num1/num2;
-        return num1 + "/" + num2 + "=" + num3;
+        if (num2 == 0) {
+            return  "Делить на ноль нельзя!";
+        } else return num1 + "/" + num2 + "=" + num3;
     }
 }
